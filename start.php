810a19a9312973f10bb1b92a8d93bdb95448367e
Wadamir <domus159@gmail.com>
2023-05-07 18:52:36 +0300

    fix start.php - added creation of user

--- start.php
+++ start.php
@@ -1,5 +1,11 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/init.php';
+$log_dir = __DIR__ . '/logs';
 
 $token = env('TOKEN', null);
 if (!$token) {
@@ -24,10 +30,47 @@ if (!$update) {
     exit;
 }
 
+file_put_contents($log_dir . '/start.log', '[' . date('Y-m-d H:i:s') . '] Received: ' . file_get_contents("php://input") . PHP_EOL, FILE_APPEND);
+
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
 
 if (strpos($message, "/start") === 0) {
     $location = substr($message, 9);
-    file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=Here's the start message in ...");
+    file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=Send rss link to your channel");
+} elseif (strpos($message, "https://www.upwork.com/") === 0) {
+    file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=Ok! I will send you updates from this channel");
+} else {
+    file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=Try again, please");
+}
+
+function createUser()
+{
+    global $log_dir;
+
+    $dbhost = env('MYSQL_HOST', 'localhost');
+    $dbuser = env('MYSQL_USER', 'root');
+    $dbpass = env('MYSQL_PASSWORD', '');
+    $dbname = env('MYSQL_DB', 'telegram_bot');
+    $table_users = env('MYSQL_TABLE_USERS', 'users');
+    $table_data = env('MYSQL_TABLE_DATA', 'data');
+    // Create connection
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+    if (!$conn) {
+        file_put_contents($log_dir . '/start.log', 'Connection failed' . PHP_EOL, FILE_APPEND);
+        die("Connection failed: " . mysqli_connect_error()) . PHP_EOL;
+    }
+    $sql = "INSERT INTO users (name, is_deleted, chat_id, link, refresh_time, last_update, date_added) VALUES ('', 0, '', '', 5, NOW(), NOW())";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        $last_id = mysqli_insert_id($conn);
+        file_put_contents($log_dir . '/start.log', "New record created successfully. Last inserted ID is: " . $last_id . PHP_EOL, FILE_APPEND);
+        echo "New record created successfully. Last inserted ID is: " . $last_id . PHP_EOL;
+    } else {
+        file_put_contents($log_dir . '/start.log', "Error: " . $sql . PHP_EOL . mysqli_error($conn) . PHP_EOL, FILE_APPEND);
+        echo "Error: " . $sql . PHP_EOL . mysqli_error($conn) . PHP_EOL;
+    }
+    // Close connection
+    file_put_contents($log_dir . '/start.log', '[' . date('Y-m-d H:i:s') . '] Close connection' . PHP_EOL . PHP_EOL, FILE_APPEND);
+    mysqli_close($conn);
 }
