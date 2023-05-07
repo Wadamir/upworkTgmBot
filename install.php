4592199077be88310b14f281caa5dced01af3d2b
Wadamir <domus159@gmail.com>
2023-05-07 18:07:31 +0300

    added install.php

---
+++ install.php
@@ -0,0 +1,104 @@
+<?php
+/*
+Create database for tgm bot
+*******************************************
+1. Create database
+2. Create table users
+3. Create table data
+4. Close connection
+*/
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/init.php';
+$log_dir = __DIR__ . '/logs';
+
+// 1. Create database
+file_put_contents($log_dir . '/install.log', '[' . date('Y-m-d H:i:s') . '] 1. Create database' . PHP_EOL, FILE_APPEND);
+
+$dbhost = env('MYSQL_HOST', 'localhost');
+$dbuser = env('MYSQL_USER', 'root');
+$dbpass = env('MYSQL_PASSWORD', '');
+$dbname = env('MYSQL_DB', 'telegram_bot');
+$table_users = env('MYSQL_TABLE_USERS', 'users');
+$table_data = env('MYSQL_TABLE_DATA', 'data');
+
+// Create connection
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+if (!$conn) {
+    file_put_contents($log_dir . '/install.log', 'Connection failed' . PHP_EOL, FILE_APPEND);
+    die("Connection failed: " . mysqli_connect_error()) . PHP_EOL;
+}
+
+// Create database
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+if (mysqli_query($conn, $sql)) {
+    file_put_contents($log_dir . '/install.log', "Database $dbname created successfully" . PHP_EOL, FILE_APPEND);
+    echo "Database $dbname created successfully" . PHP_EOL;
+} else {
+    file_put_contents($log_dir . '/install.log', "Error creating database $dbname" . PHP_EOL, FILE_APPEND);
+    echo "Error creating database $dbname: " . mysqli_error($conn) . PHP_EOL;
+}
+
+// 2. Create table users
+file_put_contents($log_dir . '/install.log', '[' . date('Y-m-d H:i:s') . '] 2. Create table ' . $table_users . PHP_EOL, FILE_APPEND);
+$sql = "CREATE TABLE IF NOT EXISTS $table_users (
+        `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `name` varchar(256) DEFAULT NULL,
+        `is_deleted` tinyint(1) DEFAULT NULL,
+        `chat_id` varchar(256) DEFAULT NULL,
+        `link` varchar(256) DEFAULT NULL,
+        `refresh_time` bigint DEFAULT '5',
+        `last_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        `date_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+    ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
+
+if (!mysqli_select_db($conn, $dbname)) {
+    file_put_contents($log_dir . '/install.log', 'Database NOT SELECTED' . PHP_EOL, FILE_APPEND);
+    die("Connection failed: " . mysqli_connect_error()) . PHP_EOL;
+    echo "Database NOT SELECTED" . PHP_EOL;
+}
+
+if (mysqli_query($conn, $sql)) {
+    file_put_contents($log_dir . '/install.log', "Table $table_users created successfully" . PHP_EOL, FILE_APPEND);
+    echo "Table $table_users created successfully" . PHP_EOL;
+} else {
+    file_put_contents($log_dir . '/install.log', "Error creating table $table_users: " . mysqli_error($conn) . PHP_EOL, FILE_APPEND);
+    echo "Error creating table $table_users: " . mysqli_error($conn) . PHP_EOL;
+}
+
+// 3. Create table data
+file_put_contents($log_dir . '/install.log', '[' . date('Y-m-d H:i:s') . '] 3. Create table ' . $table_data . PHP_EOL, FILE_APPEND);
+$sql = "CREATE TABLE IF NOT EXISTS $table_data (
+      `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      `category` varchar(255) DEFAULT NULL,
+      `skills` varchar(255) DEFAULT NULL,
+      `country` varchar(255) DEFAULT NULL,
+      `link` varchar(255) DEFAULT NULL,
+      `budget` bigint DEFAULT NULL,
+      `hourly_min` bigint DEFAULT NULL,
+      `hourly_max` bigint DEFAULT NULL,
+      `description` text,
+      `posted_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      `date_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+    ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
+
+if (!mysqli_select_db($conn, $dbname)) {
+    file_put_contents($log_dir . '/install.log', 'Database NOT SELECTED' . PHP_EOL, FILE_APPEND);
+    die("Connection failed: " . mysqli_connect_error()) . PHP_EOL;
+    echo "Database NOT SELECTED" . PHP_EOL;
+}
+
+if (mysqli_query($conn, $sql)) {
+    file_put_contents($log_dir . '/install.log', "Table $table_data created successfully" . PHP_EOL, FILE_APPEND);
+    echo "Table $table_data created successfully" . PHP_EOL;
+} else {
+    file_put_contents($log_dir . '/install.log', "Error creating table $table_data: " . mysqli_error($conn) . PHP_EOL, FILE_APPEND);
+    echo "Error creating table $table_data: " . mysqli_error($conn) . PHP_EOL;
+}
+
+// 4. Close connection
+file_put_contents($log_dir . '/install.log', '[' . date('Y-m-d H:i:s') . '] 4. Close connection' . PHP_EOL . PHP_EOL, FILE_APPEND);
+mysqli_close($conn);
