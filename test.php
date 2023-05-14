b30804791a81223c7d2449dee10e765ed178dc34
Wadamir <domus159@gmail.com>
2023-05-14 12:56:26 +0300

    fix api

--- test.php
+++ test.php
@@ -14,9 +14,7 @@ if (!$token) {
     throw new ErrorException('Token not found!');
 }
 
-$bot = new \TelegramBot\Api\BotApi($token);
-
-$path = "https://api.telegram.org/bot$token";
+// $path = "https://api.telegram.org/bot$token";
 
 $dbhost = env('MYSQL_HOST', 'localhost');
 $dbuser = env('MYSQL_USER', 'root');
@@ -34,67 +32,74 @@ if (!$conn) {
 $sql = "SELECT * FROM $table_users WHERE link IS NOT NULL";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
-    file_put_contents($log_dir . '/test.log', ' | Links exists! ', FILE_APPEND);
-    $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    foreach ($rows as $row) {
-        $link = $row['link'];
-        $chatId = $row['chat_id'];
-        $chatId = intval('443r34r34rf');
-        $refresh_time = $row['refresh_time'] * 60;
-        if ($refresh_time == 0) {
-            $refresh_time = 300;
-        }
-
-        $message = "Link: $link\nChat ID: $chatId\nRefresh time: $refresh_time";
-
-        $url = $path . "/sendmessage?chat_id=" . $chatId . "&text=" . urlencode($message) . "&parse_mode=HTML";
-        $response = file_get_contents($url);
-        $response = json_decode($response, true);
-        if ($response['ok'] === true) {
-            file_put_contents($log_dir . '/test.log', ' | Response - ok', FILE_APPEND);
-            // Update sent_to_user
-            $sql = "UPDATE $table_data SET sent_to_user = 1 WHERE link = '$link'";
-            if (!mysqli_query($conn, $sql)) {
-                file_put_contents($log_dir . '/test.log', ' | Error: ' . mysqli_error($conn), FILE_APPEND);
+    try {
+        $bot = new \TelegramBot\Api\BotApi($token);
+        file_put_contents($log_dir . '/test.log', ' | Links exists! ', FILE_APPEND);
+        $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        foreach ($rows as $row) {
+            $link = $row['link'];
+            $chatId = $row['chat_id'];
+            // $chatId = intval('443r34r34rf');
+            $refresh_time = $row['refresh_time'] * 60;
+            if ($refresh_time == 0) {
+                $refresh_time = 300;
             }
-        } else {
-            file_put_contents($log_dir . '/test.log', ' | Response - error!', FILE_APPEND);
-        }
 
-        /*
-        // Send message
-        $messageText = "Link: $link\nChat ID: $chatId\nRefresh time: $refresh_time";
-        $messageResponse = $bot->sendMessage($chatId, $messageText);
-        var_dump($messageResponse);
-        file_put_contents($log_dir . '/test.log', ' | Message sent - ' . json_encode($messageResponse), FILE_APPEND);
-        /*
-        // Send document
-        $document = new \CURLFile('document.txt');
-        $bot->sendDocument($chatId, $document);
+            /*
+            $message = "Link: $link\nChat ID: $chatId\nRefresh time: $refresh_time";
 
-        // Send message with reply keyboard
-        $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup(array(array("one", "two", "three")), true); // true for one-time keyboard
-        $bot->sendMessage($chatId, $messageText, null, false, null, $keyboard);
+            $url = $path . "/sendmessage?chat_id=" . $chatId . "&text=" . urlencode($message) . "&parse_mode=HTML";
+            $response = file_get_contents($url);
+            $response = json_decode($response, true);
+            if ($response['ok'] === true) {
+                file_put_contents($log_dir . '/test.log', ' | Response - ok', FILE_APPEND);
+                // Update sent_to_user
+                $sql = "UPDATE $table_data SET sent_to_user = 1 WHERE link = '$link'";
+                if (!mysqli_query($conn, $sql)) {
+                    file_put_contents($log_dir . '/test.log', ' | Error: ' . mysqli_error($conn), FILE_APPEND);
+                }
+            } else {
+                file_put_contents($log_dir . '/test.log', ' | Response - error!', FILE_APPEND);
+            }
+            */
 
-        // Send message with inline keyboard
-        $keyboard = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup(
-            [
+            // Send message
+            $messageText = "Link: $link\nChat ID: $chatId\nRefresh time: $refresh_time";
+            $messageResponse = $bot->sendMessage($chatId, $messageText);
+            var_dump($messageResponse);
+            file_put_contents($log_dir . '/test.log', ' | Message sent - ' . json_encode($messageResponse), FILE_APPEND);
+            /*
+            // Send document
+            $document = new \CURLFile('document.txt');
+            $bot->sendDocument($chatId, $document);
+
+            // Send message with reply keyboard
+            $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup(array(array("one", "two", "three")), true); // true for one-time keyboard
+            $bot->sendMessage($chatId, $messageText, null, false, null, $keyboard);
+
+            // Send message with inline keyboard
+            $keyboard = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup(
                 [
-                    ['text' => 'link', 'url' => 'https://core.telegram.org']
+                    [
+                        ['text' => 'link', 'url' => 'https://core.telegram.org']
+                    ]
                 ]
-            ]
-        );
-        $bot->sendMessage($chatId, $messageText, null, false, null, $keyboard);
+            );
+            $bot->sendMessage($chatId, $messageText, null, false, null, $keyboard);
 
-        // Send media group
-        $media = new \TelegramBot\Api\Types\InputMedia\ArrayOfInputMedia();
-        $media->addItem(new TelegramBot\Api\Types\InputMedia\InputMediaPhoto('https://avatars3.githubusercontent.com/u/9335727'));
-        $media->addItem(new TelegramBot\Api\Types\InputMedia\InputMediaPhoto('https://avatars3.githubusercontent.com/u/9335727'));
-        $bot->sendMediaGroup($chatId, $media);
-        // Same for video
-        $media = new \TelegramBot\Api\Types\InputMedia\ArrayOfInputMedia();
-        $media->addItem(new TelegramBot\Api\Types\InputMedia\InputMediaVideo('http://clips.vorwaerts-gmbh.de/VfE_html5.mp4'));
-        $bot->sendMediaGroup($chatId, $media);
-        */
+            // Send media group
+            $media = new \TelegramBot\Api\Types\InputMedia\ArrayOfInputMedia();
+            $media->addItem(new TelegramBot\Api\Types\InputMedia\InputMediaPhoto('https://avatars3.githubusercontent.com/u/9335727'));
+            $media->addItem(new TelegramBot\Api\Types\InputMedia\InputMediaPhoto('https://avatars3.githubusercontent.com/u/9335727'));
+            $bot->sendMediaGroup($chatId, $media);
+            // Same for video
+            $media = new \TelegramBot\Api\Types\InputMedia\ArrayOfInputMedia();
+            $media->addItem(new TelegramBot\Api\Types\InputMedia\InputMediaVideo('http://clips.vorwaerts-gmbh.de/VfE_html5.mp4'));
+            $bot->sendMediaGroup($chatId, $media);
+            */
+        }
+        file_put_contents($log_dir . '/test.log', ' | End' . PHP_EOL, FILE_APPEND);
+    } catch (\TelegramBot\Api\Exception $e) {
+        file_put_contents($log_dir . '/test.log', ' | ' . $e->getMessage() . PHP_EOL, FILE_APPEND);
     }
 }
