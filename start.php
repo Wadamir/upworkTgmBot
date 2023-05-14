cca923b0b04f58e1c615881eebf99f8f528cc599
Wadamir <domus159@gmail.com>
2023-05-15 01:03:09 +0300

    fix start

--- start.php
+++ start.php
@@ -116,14 +116,24 @@ if (strpos($message, "/start") === 0 && $message === '/start' && $user_data['is_
             $rss_links = array();
             $buttons = array();
             foreach ($existing_links as $key => $value) {
+                $buttons[] = [
+                    'text' => $key + 1,
+                    'callback_data' => $value
+                ];
                 $rss_links[] = $key + 1 . '. ' . $value;
-                $buttons[] = $key + 1 . '. ' . $value;
             }
             $existing_links = implode("\n", $rss_links);
             $messageText = "You have " . $total_links . " RSS links:\n" . $existing_links . "\nIf you want to remove your RSS link press the button.";
-            // Send message with reply keyboard
-            $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup(array($buttons), true); // true for one-time keyboard
+            // Send message with inline keyboard
+            $keyboard = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup(
+                [
+                    $buttons
+                ]
+            );
             $bot->sendMessage($chatId, $messageText, null, false, null, $keyboard);
+            // Send message with reply keyboard
+            // $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup(array($buttons), true); // true for one-time keyboard
+            // $bot->sendMessage($chatId, $messageText, null, false, null, $keyboard);
             // file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=Send here your RSS link to remove it");
         } catch (Exception $e) {
             file_put_contents($log_dir . '/start.log', ' | ' . $e->getMessage(), FILE_APPEND);
